86422e3ea4237ffeec6e17908cea3e22beb7cabf
TortoisegitAnupam <analyticsprojects10@gmail.com>
2022-10-20 21:43:28 +0530

    notes

--- Visualizations/pythonFile.py
+++ Visualizations/pythonFile.py
@@ -1,25 +1,24 @@
-import pandas as pd
-import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
-pd.set_option("display.max_columns",50)
-pd.set_option("display.width", 500)
-df = pd.read_csv("iris_dataset.csv")
+import pandas as pd
 
-#print(df[df['species']=='setosa']['sepal_length'].sum())
-
-allSpecies=[]
-allSpeciesSepal_length=[]
-for x in (df['species'].unique()):
-    allSpecies.append(x)
-
-for x in allSpecies:
-    allSpeciesSepal_length.append(df[df['species']=='{}'.format(x)]['sepal_length'].sum())
-
-
-print(allSpecies, allSpeciesSepal_length)
-totalSepalLength = np.sum(allSpeciesSepal_length)
-print(totalSepalLength)
-plt.bar(allSpecies,allSpeciesSepal_length)
-plt.pie(allSpeciesSepal_length, labels=allSpecies, autopct='%1.1f%%', shadow=True,startangle=90 )
+wine_qty = pd.read_csv("winequality.csv", delimiter=";")
+iris = pd.read_csv("iris_dataset.csv", delimiter=',')
+iris_mean = iris.mean()
+iris_std = iris.std()
+fig, axs = plt.subplots(2, 3, figsize=(10, 8))
+axs[0, 0].hist(wine_qty['alcohol'])
+axs[0, 0].set_title("Title Alcohol Frequency")
+axs[0, 0].set_xlabel('Alcohol Bin')
+axs[0, 0].set_ylabel("Frequency")
+axs[0, 1].plot(iris['sepal_length'])
+axs[0, 1].set_title("Sepal L Frequency")
+axs[0, 1].set_xlabel('sepal_length')
+axs[0, 1].set_ylabel("sepal frequency")
+axs[1, 0].scatter(iris['petal_length'], iris['petal_width'],
+                  c=iris['species'].map({'setosa': 0, 'versicolor': 1, 'virginica': 2}))
+axs[1, 0].set(title='Scatter', xlabel='Petal Len', ylabel='Petal Wd')
+axs[1, 1].bar(['sepal_l', 'sepal_w', 'petal_l', 'petal_w'], iris_mean, yerr=iris_std)
+axs[1, 1].set(title='Bar', xlabel='category', ylabel='Category Mean')
+plt.suptitle("Subplots")
+plt.tight_layout(pad=3, w_pad=1, h_pad=1)
 plt.show()
