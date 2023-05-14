acf42a4840242dad87292947b89b33171121af72
Anupam <analyticsprojects10@gmail.com>
2023-05-14 08:48:41 +0530

    added .py file for series

--- PandasNumpySeriesDataFrame/PandasNumpy.py
+++ PandasNumpySeriesDataFrame/PandasNumpy.py
@@ -1,2 +1,10 @@
 import pandas as pd
 print("series")
+
+lis = ['l1','l2','l3','l4']
+print(lis)
+l1 = pd.Series(lis)
+print(l1)
+
+dict1 = {'k1':'v1','k2':'v2','k3':'v3','k4':'v4','k5':'v5',}
+print(pd.Series(dict1))
