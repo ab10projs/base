58ad6f4126a278274b357c39f85cade997a7c11d
ab10projs <analyticsprojects10@gmail.com>
2026-06-13 14:54:31 +0530

    tabs added

--- cursorFolder/tools/dash/refreshPerSec.py
+++ cursorFolder/tools/dash/refreshPerSec.py
@@ -86,107 +86,118 @@ app.layout = dbc.Container(
     className="p-3",
     children=[
 
-        dbc.Row(
-            dbc.Col(
-                html.H2(
-                    "Live Data Streaming",
-                    className="text-center mb-4"
-                )
-            )
-        ),
-
-dbc.Row(
-    [
-        # Left Scatter Plot
-        dbc.Col(
-            dbc.Card(
-                dbc.CardBody(
-                    dcc.Graph(
-                        id="scatter-chart",
-                        style={"height": "350px"}
-                    )
-                )
-            ),
-            width=4,
-        ),
-
-        # Middle Column
-        dbc.Col(
+        dbc.Tabs(
             [
-                dbc.Card(
-                    dbc.CardBody(
-                        dcc.Graph(
-                            id="line-chart-1",
-                            style={"height": "150px"}
-                        )
-                    ),
-                    className="mb-3",
+                # Tab 1
+                dbc.Tab(
+                    label="Dashboard",
+                    children=[
+
+                        dbc.Row(
+                            dbc.Col(
+                                html.H2(
+                                    "Live Data Streaming",
+                                    className="text-center mb-4"
+                                )
+                            )
+                        ),
+
+                        dbc.Row(
+                            [
+                                # Left Scatter Plot
+                                dbc.Col(
+                                    dbc.Card(
+                                        dbc.CardBody(
+                                            dcc.Graph(
+                                                id="scatter-chart",
+                                                style={"height": "250px"}
+                                            )
+                                        )
+                                    ),
+                                    width=4,
+                                ),
+
+                                # Middle Column
+                                dbc.Col(
+                                    [
+                                        dbc.Card(
+                                            dbc.CardBody(
+                                                dcc.Graph(
+                                                    id="line-chart-1",
+                                                    style={"height": "100px"}
+                                                )
+                                            ),
+                                            className="mb-3",
+                                        ),
+
+                                        dbc.Card(
+                                            dbc.CardBody(
+                                                dcc.Graph(
+                                                    id="line-chart-2",
+                                                    style={"height": "100px"}
+                                                )
+                                            )
+                                        ),
+                                    ],
+                                    width=4,
+                                ),
+
+                                # Right Column
+                                dbc.Col(
+                                    [
+                                        dbc.Card(
+                                            dbc.CardBody(
+                                                dcc.Graph(
+                                                    id="line-chart-3",
+                                                    style={"height": "100px"}
+                                                )
+                                            ),
+                                            className="mb-3",
+                                        ),
+
+                                        dbc.Card(
+                                            dbc.CardBody(
+                                                dcc.Graph(
+                                                    id="line-chart-4",
+                                                    style={"height": "100px"}
+                                                )
+                                            )
+                                        ),
+                                    ],
+                                    width=4,
+                                ),
+                            ],
+                            className="g-3",
+                        ),
+                    ],
                 ),
 
-                dbc.Card(
-                    dbc.CardBody(
-                        dcc.Graph(
-                            id="line-chart-2",
-                            style={"height": "150px"}
-                        )
-                    )
+                # Tab 2
+                dbc.Tab(
+                    label="Tab 2",
+                    children=[
+                        html.Div("Coming Soon...", className="p-4")
+                    ],
                 ),
-            ],
-            width=4,
+
+                # Tab 3
+                dbc.Tab(
+                    label="Tab 3",
+                    children=[
+                        html.Div("Coming Soon...", className="p-4")
+                    ],
+                ),
+            ]
         ),
-
-        # Right Column
-        dbc.Col(
-            [
-                dbc.Card(
-                    dbc.CardBody(
-                        dcc.Graph(
-                            id="line-chart-3",
-                            style={"height": "150px"}
-                        )
-                    ),
-                    className="mb-3",
-                ),
-
-                dbc.Card(
-                    dbc.CardBody(
-                        dcc.Graph(
-                            id="line-chart-4",
-                            style={"height": "150px"}
-                        )
-                    )
-                ),
-            ],
-            width=4,
-        ),
-    ],
-    className="g-3",
-),
 
         dcc.Interval(
             id="interval-component",
-            interval=300,
+            interval=500,
             n_intervals=0,
         ),
     ],
 )
 
-
-
-
-
-# #---- Old Layout Starts Here ----
-# app.layout = html.Div([
-#     dcc.Graph(id="scatter-chart"),
-#     dcc.Graph(id="line-chart"),
-#     # Fires every second
-#     dcc.Interval(
-#         id="interval-component",
-#         interval=350,  # milliseconds
-#         n_intervals=0
-#     )
-# ])
-# #---- Old Layout Ends Here ----
 
 
 @app.callback(
@@ -213,7 +224,7 @@ def update_chart(n_intervals):
     )
     #--- this filtered dataframe is common to all the plots ending here----
 
-    #--- Fig1 is for scatter plot live simulation----
+    #--- Fig1 is for scatter plot live simulation---- start
     fig1 = go.Figure(
         go.Scatter(
             x=filtered["SYMBOL"].to_list()[:5000],
@@ -250,17 +261,12 @@ def update_chart(n_intervals):
     margin=dict(l=20, r=20, t=30, b=20),
     template="plotly_white",
 )
+#--- Fig1 is for scatter plot live simulation---- end
 
-    # fig1.update_layout(
-    # width=600,   # chart width in pixels
-    # height=400   # chart height in pixels
-    # )
-    #--- Fig1 is for scatter plot live simulation----
-
-    #--- Fig 2 is line plot of a particular symbol----
-    filteredScrip1 = df.filter([(pl.col("SYMBOL") == "ACC") & (pl.col("DATE1") <= xListDate[counter])])
+#--- Fig2 is for line plot of a particular symbol---- start
+    filteredScrip1 = df.filter([(pl.col("SYMBOL") == "ICICIBANK") & (pl.col("DATE1") <= xListDate[counter])])
     fig2 = go.Figure(
-        go.Line(
+        go.Scatter(
             x=filteredScrip1["DATE1"].to_list(),
             y=filteredScrip1["CLOSE_PRICE"].to_list(),
             mode='markers',
@@ -276,9 +282,72 @@ def update_chart(n_intervals):
     margin=dict(l=20, r=20, t=30, b=20),
     template="plotly_white",
 )
-    #--- Fig 2 is line plot of a particular symbol----
+#--- Fig2 is for line plot of a particular symbol---- end 
+#--- Fig3 is for line plot of a particular symbol---- start
+    filteredScrip3 = df.filter([(pl.col("SYMBOL") == "RELIANCE") & (pl.col("DATE1") <= xListDate[counter])])
+    fig3 = go.Figure(
+        go.Scatter(
+            x=filteredScrip3["DATE1"].to_list(),
+            y=filteredScrip3["CLOSE_PRICE"].to_list(),
+            mode='markers',
+            marker=dict(size=3,
+                        color= filteredScrip3["CLOSE_PRICE"],
+                        colorscale="Viridis",  
+                        opacity=0.9, 
+                        line=dict(width=0.8)  
+                        ),
+        )
+    )
+    fig3.update_layout(
+    margin=dict(l=20, r=20, t=30, b=20),
+    template="plotly_white",
+)
+#--- Fig3 is for line plot of a particular symbol---- start
+#--- Fig4 is for line plot of a particular symbol---- start
+    filteredScrip4 = df.filter([(pl.col("SYMBOL") == "BHEL") & (pl.col("DATE1") <= xListDate[counter])])
+    fig4 = go.Figure(
+        go.Scatter(
+            x=filteredScrip4["DATE1"].to_list(),
+            y=filteredScrip4["CLOSE_PRICE"].to_list(),
+            mode='markers',
+            marker=dict(size=3,
+                        color= filteredScrip4["CLOSE_PRICE"],
+                        colorscale="Viridis",  
+                        opacity=0.9, 
+                        line=dict(width=0.8)  
+                        ),
+        )
+    )
+    fig4.update_layout(
+    margin=dict(l=20, r=20, t=30, b=20),
+    template="plotly_white",
+)
+#--- Fig4 is for line plot of a particular symbol---- end
 
-    return fig1, fig2 , fig2  ,fig2, fig2
+
+
+#--- Fig5 is for line plot of a particular symbol---- start
+    filteredScrip5 = df.filter([(pl.col("SYMBOL") == "HAL") & (pl.col("DATE1") <= xListDate[counter])])
+    fig5 = go.Figure(
+        go.Scatter(
+            x=filteredScrip5["DATE1"].to_list(),
+            y=filteredScrip5["CLOSE_PRICE"].to_list(),
+            mode='markers',
+            marker=dict(size=3,
+                        color= filteredScrip5["CLOSE_PRICE"],
+                        colorscale="Viridis",  
+                        opacity=0.9, 
+                        line=dict(width=0.8)  
+                        ),
+        )
+    )
+    fig5.update_layout(
+    margin=dict(l=20, r=20, t=30, b=20),
+    template="plotly_white",
+)
+#--- Fig5 is for line plot of a particular symbol---- end
+
+    return fig1, fig2 , fig3  ,fig4, fig5
 
 if __name__ == "__main__":
     app.run(debug=True)
